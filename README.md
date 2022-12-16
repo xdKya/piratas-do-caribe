2655fe839c4b4942fd56c69b15bb2f393b5ff441
xdKya <90745676+xdKya@users.noreply.github.com>
2022-12-16 11:29:17 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# piratas-do-caribe
+# PiratesInvasionStage-6
+
+adding sounds and score
---
+++ cannon.js
@@ -0,0 +1,18 @@
+class Cannon {
+  //propriedades da classe
+  constructor(x, y, w, h, angle) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.angle = angle;
+
+    //images
+  }
+
+  //funções da classe
+  show() {
+    //base do canhao
+    //topo do canhao
+  }
+}
